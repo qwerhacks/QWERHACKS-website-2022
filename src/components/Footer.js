951f03d49453da3502df62c76339ba9a4be08a3d
Tanya Zhong <tanyazhong1@gmail.com>
2021-08-25 14:27:26 -0700

    favicon and footer reformat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,33 +7,36 @@ function Footer() {
       <div class="footer">
          <div class="footer-container" >
             <div class="footer-section">
-                  <img src={require("../img/qh_logo.png")} alt="website logo" class="footer-logo" />
+               <img src={require("../img/qh_logo.png")} alt="website logo" class="footer-logo" />
             </div>
             <div class="footer-section">
+            <div>social media:</div>
+
                <div class="footer-social">
                   <div class="footer-icon-wrapper">
                      <a class="footer-icon" href="https://www.facebook.com/qwerhacks/" target="_blank" rel="noopener noreferrer">
-                        <img src={require("../img/fb-icon.png")} alt="facebook logo" width="100%" />
+                        <img src={require("../img/facebook.png")} alt="facebook logo" width="100%" />
                      </a>
                   </div>
                   <div class="footer-icon-wrapper">
                      <a class="footer-icon" href="https://www.instagram.com/qwerhacks/" target="_blank" rel="noopener noreferrer" >
-                        <img src={require("../img/ig-icon.png")} alt="instagram logo" width="100%" />
-                     </a>
-                  </div>
-                  <div class="footer-icon-wrapper">
-                     <a class="footer-icon" href="mailto:qwerhacks@gmail.com">
-                        <img src={require("../img/email-icon.png")} alt="email logo" width="100%" />
+                        <img src={require("../img/instagram.png")} alt="instagram logo" width="100%" />
                      </a>
                   </div>
                   <div class="footer-icon-wrapper">
                      <a class="footer-icon" href="https://twitter.com/qwerhacks" target="_blank" rel="noopener noreferrer" >
-                        <img src={require("../img/icon-twitter.png")} alt="twitter logo" width="100%" />
+                        <img src={require("../img/twitter.png")} alt="twitter logo" width="100%" />
+                     </a>
+                  </div>
+                  <div class="footer-icon-wrapper">
+                     <a class="footer-icon" href="mailto:qwerhacks@gmail.com">
+                        <img src={require("../img/email3.png")} alt="email logo" width="100%" />
                      </a>
                   </div>
                </div>
             </div>
             <div class="footer-section">
+               <div>etc:</div>
                <a class="footer-link" href="https://drive.google.com/file/d/1Q-oz3N8M9OQd0Assxkzp0lhJrG9K_yp0/view" target="_blank" rel="noopener noreferrer">sponsorship packet (color)</a>
                <a class="footer-link" href="https://drive.google.com/file/d/15rca4077W_vbpksJKcfUJlV75e-Mt_OC/view" target="_blank" rel="noopener noreferrer">sponsorship packet (b/w)</a>
                <a class="footer-link" href="https://docs.google.com/forms/d/e/1FAIpQLScLI58PM-83LkVisDNw7MtaODQpSp36QtJEDjhsgAgmd9XQNA/viewform" target="_blank" rel="noopener noreferrer">interested in judging, mentoring, or volunteering?</a>
