d43038e323ba63ce0ba650320985b04c936ac4e8
Tanya Zhong <tanyazhong1@gmail.com>
2021-08-30 19:41:13 -0700

    Updates

---
+++ precache-manifest.e64a42c1a80a5165d9828c98caf1341f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0765147ecce098de6a07d7dcfbc26082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3ae145a87044ca76b6f",
+    "url": "/static/css/main.ce9f9a59.chunk.css"
+  },
+  {
+    "revision": "ad227dd72f0bc60b25c7",
+    "url": "/static/js/2.95c0d4b1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.95c0d4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ae145a87044ca76b6f",
+    "url": "/static/js/main.a61eb251.chunk.js"
+  },
+  {
+    "revision": "a585c8e96928d892102d",
+    "url": "/static/js/runtime-main.68e7579c.js"
+  },
+  {
+    "revision": "1bb17e6e1710668d8dccdd05d6efbb7e",
+    "url": "/static/media/AvenirNext-DemiBold.1bb17e6e.woff2"
+  },
+  {
+    "revision": "ab1da0849481f823698ff9cb0dba6785",
+    "url": "/static/media/AvenirNext-DemiBold.ab1da084.woff"
+  },
+  {
+    "revision": "2c5f15242911add8502d74c7143fb889",
+    "url": "/static/media/a&e_circle.2c5f1524.png"
+  },
+  {
+    "revision": "6c9a37e4ef6f99c008271c4ed8f5b8fa",
+    "url": "/static/media/c&c_circle.6c9a37e4.png"
+  },
+  {
+    "revision": "bdf8bb1a51c07d97354fa8cbdc2ff2d2",
+    "url": "/static/media/email3.bdf8bb1a.png"
+  },
+  {
+    "revision": "ecf2a1982396bb4103b6f94ab19a6fe4",
+    "url": "/static/media/gradient-extra-long.ecf2a198.png"
+  },
+  {
+    "revision": "4a7eda915a86edfcef3465d16687039e",
+    "url": "/static/media/h&w_circle.4a7eda91.png"
+  },
+  {
+    "revision": "1d3bf019927dbe6af973dd3d32db862f",
+    "url": "/static/media/instagram.1d3bf019.png"
+  },
+  {
+    "revision": "0c16072a500d9d56d6d40b506db9a001",
+    "url": "/static/media/kenokylight-3zezl-webfont.0c16072a.woff"
+  },
+  {
+    "revision": "fc47da7eea08762f104318738f944cd7",
+    "url": "/static/media/kenokylight-3zezl-webfont.fc47da7e.woff2"
+  },
+  {
+    "revision": "4d954b7c646861b80e7cdccca5bc0640",
+    "url": "/static/media/l&g_circle.4d954b7c.png"
+  },
+  {
+    "revision": "94db60ef0da1fd22e4d96ac2d11ef063",
+    "url": "/static/media/qh_logo.94db60ef.png"
+  },
+  {
+    "revision": "d326da415c1f2c4d74908c87020aa482",
+    "url": "/static/media/qh_logo_circle.d326da41.png"
+  }
+]);
